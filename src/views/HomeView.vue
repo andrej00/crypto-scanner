b14149d5c766ee0c6c5c943b01368e6f16c64384
andrej00 <prskalo.andrej@digitalrepublic.ba>
2022-04-02 19:13:35 +0200

    added line chart to tickers

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	import LineChart from '@/components/LineChart.vue'
+	import { ref, reactive } from 'vue'
+
+	let data = ref<number[]>([])
+
+	setInterval(() => {
+		data.value.push(Math.round(Math.random() * 100))
+		if (data.value.length > 20) {
+			data.value = data.value.slice(data.value.length - 20, data.value.length)
+		}
+		console.log(data.value.length)
+	}, 500)
+</script>
 
 <template>
-	<div class="flex mt-64">
-		<div class="m-auto">
-		<router-link
-			:to="{ name: 'tickers' }"
-			class="text-5xl m-auto text-green-500"
-		>
-			TICKERS
-		</router-link>
-		</div>
-	</div>
+	<!-- <div class="flex mt-64"> -->
+		<!-- <div class="m-auto"> -->
+			<router-link
+				:to="{ name: 'tickers' }"
+				class="text-5xl m-auto text-green-500"
+			>
+				TICKERS
+			</router-link>
+			<LineChart
+				:data="data"
+			/>
+		<!-- </div> -->
+	<!-- </div> -->
 </template>
--- src/views/Tickers.vue
+++ src/views/Tickers.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+	import LineChart from '@/components/LineChart.vue'
 	import PageLoader from '@/components/PageLoader.vue'
 	import SearchIcon from '@/components/icons/SearchIcon.vue'
 
@@ -17,6 +18,16 @@
 	} = storeToRefs(binanceStore)
 
 	let search = ref('')
+	let data = ref<number[]>([])
+
+	setInterval(() => {
+		data.value.push(Math.round(Math.random() * 100))
+		if (data.value.length > 20) {
+			data.value = data.value.slice(data.value.length - 20, data.value.length)
+		}
+		console.log(data.value.length)
+	}, 500)
+
 	const favoriteCoinsList = ref(['BTCUSDT', 'ETHUSDT'])
 	console.log(favoriteCoinsList.value.includes('BTCUSDT'))
 
@@ -75,6 +86,9 @@
 					<p class="pb-1">{{ Math.round(coin.v) }}</p>
 					<p>{{ coin.P }}%</p>
 				</span>
+				<LineChart
+					:data="data"
+				/>
 				<button
 					class="border-2 text-sm border-indigo-200/60 text-indigo-200/60 flex m-auto my-3 px-2 py-0.5"
 					@click="router.push({name: 'ticker-chart', params: {id: coin.s}})"
